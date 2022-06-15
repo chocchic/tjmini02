454c435d5657f9cd5f276af37dbabb7aa6aed4af
chocchic <60125582+chocchic@users.noreply.github.com>
2022-06-15 17:22:19 +0900

    Delete tjmini02/vegekit directory

--- tjmini02/vegekit/src/main/java/com/shop/domain/CartDTO.java
+++
@@ -1,9 +0,0 @@
-package com.shop.domain;
-
-import lombok.Data;
-
-@Data
-public class CartDTO {
-	private Long pnum;
-	private Integer quantity;
-}
--- tjmini02/vegekit/src/main/java/com/shop/service/SchedulerService.java
+++
@@ -1,9 +0,0 @@
-package com.shop.service;
-
-public interface SchedulerService {
-	public void SetSale();
-	
-	public void DoSubPay();
-	
-	public void DoSubAlarm();
-}
